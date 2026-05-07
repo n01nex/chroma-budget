115a8f4fcf73db23175617b6cca17f8cdcfbd6dd
nex <nex@owlone.dev>
2026-05-07 09:21:57 +0200

    Lipgloss Experimenting for a custom colorized table part 1

---
+++ Internal/TUI/chroma_table.go
@@ -0,0 +1,7 @@
+package TUI
+
+/* PACKAGES DOCS:
+https://pkg.go.dev/charm.land/lipgloss/v2@v2.0.3
+https://pkg.go.dev/charm.land/bubbles/v2
+https://pkg.go.dev/github.com/charmbracelet/bubbletea/v2
+*/
